a4565a8c907d29b6fdd8f4d8323d287598cc10e7
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-30 23:47:43 +0530

    Add basic routing support

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import './App.css';
-
-function App() {
-    return <div className='App'>LaReCRUD</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import {Home} from './pages/home';
+import {Create} from './pages/create';
+import {Edit} from './pages/edit';
+
+function App() {
+    return (
+        <div className='App'>
+            <nav className='navbar navbar-expand navbar-darg bg-dark'>
+                <div className='navbar-nav mr-auto'>
+                    <li>
+                        <Link className='nav-link' to={'/'}>
+                            LaReCRUD
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className='nav-link' to={'/create'}>
+                            Create
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className='nav-link' to={'/edit'}>
+                            Edit
+                        </Link>
+                    </li>
+                </div>
+            </nav>
+            <div className='container'>
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/create' element={<Create />}></Route>
+                    <Route path='/edit' element={<Edit />}></Route>
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+export default App;
