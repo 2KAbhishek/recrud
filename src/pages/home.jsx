9451da8856214a63f22dd6dd789bcd5b77610959
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 00:17:20 +0530

    Fetch users from backend using axios

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,38 @@
+import {useEffect, useState} from 'react';
+import backend from '../backend';
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetchAllUsers();
+    }, []);
+    const fetchAllUsers = () => {
+        backend.get('/users').then((res) => {
+            setUsers(res.data);
+        });
+    };
+    return (
+        <main>
+            <h2>List Of Users</h2>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>edit</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </main>
+    );
+};
