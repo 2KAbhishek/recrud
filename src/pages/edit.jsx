3749ad662a114af7b92b24edfca8e0e39ed1d3a6
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 16:55:36 +0530

    Add edit logic

--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,4 +1,36 @@
+import { useState, useEffect } from "react";
+import backend from "../backend";
+import { useNavigate, useParams } from "react-router-dom";
+
 export const Edit = () => {
+  const [inputs, setInputs] = useState({});
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = () => {
+    backend.get("/users/" + id + "/edit").then((res) => {
+      setInputs({
+        name: res.data.name,
+        email: res.data.email,
+      });
+    });
+  };
+
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    backend.post("/users", inputs).then((res) => {
+      navigate("/");
+    });
+  };
+
   return (
     <div>
       <h2>Edit User</h2>
