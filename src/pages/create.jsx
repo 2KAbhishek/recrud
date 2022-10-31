d921281c1bd5993bd798b7afe7837800f65521ae
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 15:45:13 +0530

    Add create form

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,5 +1,35 @@
 export const Create = () => {
   return (
-    <div>Create</div>
-  )
-}
+    <div>
+      <h2>Create User</h2>
+      <div className="row">
+        <div className="col-sm-6 mx-auto justify-content-center">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            required
+            className="form-control m-2"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            className="form-control m-2"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            className="form-control m-2"
+          />
+          <button type="button" className="btn btn-success col-sm-6 m-2">
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
