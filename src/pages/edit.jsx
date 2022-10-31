d556d88b5bad919c71da51adc4c6169297bcf9ac
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 16:55:17 +0530

    Update edit form

--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,5 +1,36 @@
 export const Edit = () => {
   return (
-    <div>Edit</div>
-  )
-}
+    <div>
+      <h2>Edit User</h2>
+      <div className="row">
+        <div className="col-sm-6 mx-auto justify-content-center">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            required
+            className="form-control m-2"
+            value={inputs.name || ""}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            className="form-control m-2"
+            value={inputs.email || ""}
+            onChange={handleChange}
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="btn btn-info col-sm-6 m-2"
+          >
+            Update
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
